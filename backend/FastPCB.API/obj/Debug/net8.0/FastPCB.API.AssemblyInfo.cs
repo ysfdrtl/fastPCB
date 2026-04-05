0518057caf392bca1ddac8ca0043cadec317a1f8
ysfdrtl <yusuf.doruatli123@gmail.com>
2026-04-05 18:22:47 +0300

    .

--- backend/FastPCB.API/obj/Debug/net8.0/FastPCB.API.AssemblyInfo.cs
+++ backend/FastPCB.API/obj/Debug/net8.0/FastPCB.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastPCB.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c18ecb2e986c77b2c2513780851ab04b49ed065")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f6cac4bf417ab5e73bc2b1a2602fadede877d8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastPCB.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastPCB.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
